9cda2a0dda1cd23ccb39ec80cbcf9c737fc001a6
Shriram-Balaji <shrirambalaji1996@gmail.com>
2017-03-12 03:51:58 +0530

    Obtained Valid JSON Response for all style ids - Volkwagen (Really Looong RunningTime)

--- getCarReviews.py
+++ getCarReviews.py
@@ -1,15 +1,28 @@
 import json
-makes_minimal = ["volkswagen", "nissan", "mercedes-benz", "mini", "jeep", "kia", "toyota", "hyundai"]  # badcars
+import requests
+# makes_minimal = ["volkswagen", "nissan", "mercedes-benz", "mini", "jeep", "kia", "toyota", "hyundai"] 
+ # badcars
+
+makes_minimal = ["volkswagen"]
 i=0 
 style_ids = []
   
+
+def getReviews(id):
+	apiUrl = "https://api.edmunds.com/api/vehiclereviews/v2/styles/"+str(id)+"?fmt=json&api_key=sv3ga54hu77qybtxamykpbdg"
+	res = requests.get(apiUrl)
+	print res
+
+	if res.status_code == 200:	
+		with open('volkswagen-reviews.json','a') as f:
+			f.write("{},\n".format(json.dumps(res.json())))
+
 for make in makes_minimal:
   with open('data/'+make+'.json','r') as json_data:
         objects = json.load(json_data)
         for current_model in objects["models"]:
             for year in current_model["years"]:
                 for style in year["styles"]:
-                    style_ids.append(style["id"])
-                
-i+=len(style_ids)
-print 'Style IDS to parse ie. Number of Requests for all brands = '+str(i)
+                	getReviews(style["id"])
+
+       
