d99578dcb877710e03d0c808e05427708964acc4
Shriram <shrirambalaji1996@gmail.com>
2017-03-19 02:21:36 +0530

    Implemented Mongodb and Spark

--- getCarReviews.py
+++ getCarReviews.py
@@ -1,17 +1,17 @@
 import json
 import requests
-makes_minimal = ["mercedes-benz", "mini", "jeep", "kia","hyundai","toyota"] 
+makes_minimal = ["volkswagen", "nissan"] 
 i=0 
 style_ids = []
   
 
 def getReviews(make,id):
-	apiUrl = "https://api.edmunds.com/api/vehiclereviews/v2/styles/"+str(id)+"?fmt=json&api_key=sv3ga54hu77qybtxamykpbdg"
+	apiUrl = "https://api.edmunds.com/api/vehicle/v2/styles/"+str(id)+"?fmt=json&api_key=sv3ga54hu77qybtxamykpbdg"
 	res = requests.get(apiUrl)
 	print res
 
 	if res.status_code == 200:	
-		with open(make+'-reviews.json','a') as f:
+		with open(make+'-details.json','a') as f:
 			f.write("{},\n".format(json.dumps(res.json())))
 
 for make in makes_minimal:
--- sentimentAnalysis.py
+++ sentimentAnalysis.py
@@ -4,6 +4,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 from textblob import TextBlob
 import time
+from pymongo import MongoClient
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import SparkSession,SQLContext
+import pymongo
+import pymongo_spark
+
+spark = SparkSession.builder.appName("myApp").config("spark.mongodb.input.partitioner","MongoShardedPartitioner").getOrCreate()
+sc = spark.sparkContext
+sc.setLogLevel("ERROR")
+
+client = MongoClient("mongodb://localhost:27017/")
+db = client.reviews
+pymongo_spark.activate()
 
 makes_minimal = ["volkswagen","nissan","mercedes-benz", "mini", "jeep", "kia","hyundai"] 
 make_sum = []
@@ -12,7 +25,7 @@ make_sum = []
 def printSlash():
 	syms = ['\\', '|', '/', '-']
 	bs = '\b'
-	for _ in range(10):
+	for _ in range(5):
 		for symbol in syms:
 			sys.stdout.write("\b%s" % symbol)
 			sys.stdout.flush()
@@ -27,34 +40,40 @@ def findScoreForMake(scores_array,numberOfReviews):
 
 
 #for parsing json file (will be changed to rdd or pandas df in next build)
-def parseFile(fileUrl,scores_array):
+def parseFile(collectionUrl,scores_array):
 	count = 0
-	with open(fileUrl,'r') as jsonFile:
-			sent_objects = json.load(jsonFile)
-			for wrapper_obj in sent_objects["reviews"]:
-				for reviews_array in wrapper_obj["reviews"]:
-					count+=1;
-					try:
-						text_reviews = reviews_array["text"] #getting text and suggested improvement
-						sugg_improvements = reviews_array["suggestedImprovements"]
-						blob_text = TextBlob(text_reviews)
-						blob_improvements = TextBlob(sugg_improvements).sentiment.polarity
+	config = "mongodb://localhost:27017/reviews."+collectionUrl
+	mongoRDD = sc.mongoRDD(config)
+#mongoRDD Not connected.
 
-						#polarity_score = polarity(text) + polarity(suggested_improvements)
-						polarity_score = blob_text.sentiment.polarity + float(blob_improvements)
+# configuration
+	wrapper = db[collectionUrl]
+	for container in wrapper.find():
+		for wrapper_obj in container["reviews"]:
+			for reviews_array in wrapper_obj["reviews"]:
+				count+=1;
+				try:
+					text_reviews = reviews_array["text"] #getting text and suggested improvement
+					sugg_improvements = reviews_array["suggestedImprovements"]
+					blob_text = TextBlob(text_reviews)
+					blob_improvements = TextBlob(sugg_improvements).sentiment.polarity
 
-						#avg_rating given by the user.
-						average_rating = reviews_array["averageRating"]
+					#polarity_score = polarity(text) + polarity(suggested_improvements)
+					polarity_score = float(blob_text.sentiment.polarity + float(blob_improvements))/2
 
-						#obtaining fuel_economy rating
-						fuel_economy = reviews_array["ratings"][2]["value"]
+					#avg_rating given by the user.
+					average_rating = reviews_array["averageRating"]
 
-						edmunds_score = ((polarity_score) + float(average_rating))*float(fuel_economy)
-						scores_array.append(edmunds_score)
+					#obtaining fuel_economy rating
+					fuel_economy = reviews_array["ratings"][2]["value"]
 
-					except KeyError:
-						#if text params or any specified params are not available. that review is skipped
-							continue
+					edmunds_score = ((polarity_score) + float(average_rating))*float(fuel_economy)
+					scores_array.append(edmunds_score)
+
+				except KeyError:
+					#if text params or any specified params are not available. that review is skipped
+					continue
+
 	return count
    					
 
@@ -63,26 +82,25 @@ def parseFile(fileUrl,scores_array):
 def getSentiments():
 	print "Analyzing DataSet. Preparing Edmund's Score for Vehicles. This may take a while . . . .",printSlash()
 	for make in makes_minimal:
-		fileUrl = make+"-reviews.json"
+		dbUrl = make+"-reviews"
 		scores_array = []
-
 		#returns the no.of reviews.
-		numberOfReviews = parseFile(fileUrl,scores_array)
-
+		numberOfReviews = parseFile(dbUrl,scores_array)
 		findScoreForMake(scores_array,numberOfReviews)
 
 def buildGraph():
-	print "Preparing graph . . .",printSlash()
-
-	#aligning and centering graph basedd on the number of makes.
-	plt.bar(range(len(make_sum)),make_sum,align='center')
-
-	#make_names as x axis legends
-	plt.xticks(range(len(make_sum)),makes_minimal,size = 'small')
-
-	#graph rendered
-	print 'Graph rendered successfully.'
-	plt.show()
+    print "Preparing graph . . .",printSlash()
+    
+    #aligning and centering graph basedd on the number of makes.
+    plt.bar(range(len(make_sum)),make_sum,align='center')
+    
+    #make_names as x axis legends
+    plt.xticks(range(len(make_sum)),makes_minimal,size = 'small')
+    
+    #graph rendered
+    print 'Graph rendered successfully. Image generated at output_graph.png'
+    plt.savefig("output_graph.png")
+    plt.show()
 
 #helper methods
 getSentiments()
